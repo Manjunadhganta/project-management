139f9036b3d05a936bf10b12c7eef8959bbbf417
Manjunadhganta <gantamanjunadhreddy@gmail.com>
2025-07-11 12:47:51 +0530

    Navbar Done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { FaRegUserCircle } from "react-icons/fa";
+import { FcCollaboration } from "react-icons/fc";
+import { IoMenu } from "react-icons/io5";
+import { IoCloseOutline } from "react-icons/io5";
+
+
+const Navbar = () =>{
+
+    const items = [
+        'Home',
+        'About',
+        'Testimonials',
+        'Pricing',
+        'Contact Us'
+    ]
+
+    const [isOpen, setOpen] = useState(false);
+
+    return (
+
+        <>
+            <div className="w-full flex justify-center mt-4 fixed">
+                <div style={{fontFamily:'"Space Grotesk", sans-serif'}} className="mx-auto gap-20 bg-white rounded-full px-7 py-4 items-center justify-between inline-flex shadow-md">
+                    <div style={{fontFamily:'"Edu NSW ACT Cursive", cursive'}} className="font-semibold text-xl text-blue-600">
+                        <span className="flex items-center gap-2"><FcCollaboration className="text-[25px]"/>Collabify</span>
+                    </div>
+
+                    <div className="hidden lg:flex justify-center gap-4 items-center">
+                        {items.map((item, index)=>(
+                            <a key={index} className="text-lg">
+                                {item}
+                            </a>
+                        ))}
+                    </div>
+                    <div className="hidden lg:flex gap-4">
+                        <button className="px-3 py-1 border-black border rounded-lg">Sign Up</button>
+                        <button className="px-3 py-1 bg-black text-white rounded-lg flex items-center gap-2"><FaRegUserCircle /> Log in</button>
+                    </div>
+
+                    {/* Mobile Version Code */}
+
+                    <div className="lg:hidden flex justify-center items-center">
+                        <button onClick={()=>setOpen(!isOpen)}>
+                            {isOpen ? <IoCloseOutline size={28} /> : <IoMenu size={28} />}
+                        </button>
+                    </div>
+
+                    {isOpen && (
+                        <div className="absolute top-17 left-0 w-full bg-white shadow-md z-50 px-6 py-5 flex flex-col lg:hidden gap-4 items-center">
+                                {items.map((item, index)=>(
+                                    <a key={index} className="text-lg">
+                                        {item}
+                                    </a>
+                                ))}
+                            <div className="flex flex-col gap-4">
+                                <button className="px-9 py-1 border-black border-1 rounded-lg">Sign Up</button>
+                                <button className="px-9 py-1 bg-black text-white rounded-lg flex items-center gap-2"><FaRegUserCircle /> Log in</button>
+                            </div>
+                        </div>
+                    )}
+
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
